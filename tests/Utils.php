739fc040dc51897f5d454bbf90708ea59ad85448
Enrico Stahn <mail@enricostahn.com>
2016-03-16 09:51:17 -0400

    Applied fixes from StyleCI

--- tests/Utils.php
+++ tests/Utils.php
@@ -1,13 +1,23 @@
 <?php
 
+/*
+ * This file is part of the phpunit-json-assertions package.
+ *
+ * (c) Enrico Stahn <enrico.stahn@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace EnricoStahn\JsonAssert\Tests;
 
 class Utils
 {
     /**
-     * Returns the full path of the schema file
+     * Returns the full path of the schema file.
      *
      * @param string $filename The filename of the schema file
+     *
      * @return string
      */
     public static function getSchemaPath($filename)
@@ -16,9 +26,10 @@ class Utils
     }
 
     /**
-     * Returns the full path of the schema file
+     * Returns the full path of the schema file.
      *
      * @param string $filename The filename of the json file
+     *
      * @return string
      */
     public static function getJsonPath($filename)
