7e90d3da8c8480947b12a5995cca1895352931df
Enrico Stahn <mail@enricostahn.com>
2016-03-16 03:25:25 -0400

    Applied fixes from StyleCI

--- src/Integration/Symfony.php
+++ src/Integration/Symfony.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the phpunit-json-assertions package.
+ *
+ * (c) Enrico Stahn <enrico.stahn@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace EnricoStahn\JsonAssert\Integration;
 
 use EnricoStahn\JsonAssert\Assert;
